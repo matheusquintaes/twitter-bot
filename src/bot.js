7ac421e8629ae646f15ee451925a847e1ed3fa74
Matheus Quintaes <matheus.quintaes@gmail.com>
2019-11-23 22:00:26 +1300

    Set Interval

--- src/bot.js
+++ src/bot.js
@@ -6,7 +6,7 @@ const client = new Twitter(config);
 
 const frequencyLikeInMinutes = 30
 //30minutes
-// setInterval(likeBot, 1000 * 60 * frequencyLikeInMinutes);
+setInterval(likeBot, 1000 * 60 * frequencyLikeInMinutes);
 function getParams() {
   const params = {
     q: '(%23100DaysOfCode)%20-filter%3Areplies',
@@ -43,72 +43,71 @@ function likeBot() {
   
     })
   }
-
   
-function searchTweetsForLike () {
+  function searchTweetsForLike () {
 
-  client.get('search/tweets', getParams(), function(error, data) {
-  
-    if(!error) {
-  
-      const tweets = data.statuses
-
-      const sanitizedTweets = filtersTheGoodOnes(tweets)
-
-      sanitizedTweets ? tryToFavorite(sanitizedTweets) : console.log("no good tweet found 😢")
-
-    } else {
-      console.log(error) 
-    }
-  
-  })
- 
-}
-
-function filtersTheGoodOnes(tweets) {
-  
-  const arrayOfTweets = Array.from(tweets)
-
-  const containsRestrictedWords = /^((?!free|coupons|coupon).)*$/gim;
-  
-  const containsDay = /(day\d{1,2})|(day \d{1,2})|(D \d{1,2})|(D-\d{1,2})|(day-\d{1,2})|(\d{1,2}\/100+)/gim
-
-  const sanitizedTweets = arrayOfTweets.filter((tweet) => {
+    client.get('search/tweets', getParams(), function(error, data) {
     
-    return  !tweet.retweeted_status && 
-            !containsRestrictedWords.test(tweet.text) && 
-            containsDay.test(tweet.text) && 
-            tweet.entities.hashtags.length <= 5 
-  })
-
-  return sanitizedTweets
-}
-
-function tryToFavorite(tweets) {
-
-  for (let i = 0; i < tweets.length; i++) {
-
-    let tweetId = { id: tweets[i].id_str }
+      if(!error) {
     
-    client.post('favorites/create', tweetId, function (error, response) {
-      if (!error) {
-        
-        let username = response.user.screen_name;
-        let tweetIdString = response.id_str;
+        const tweets = data.statuses
 
-        showFavoriteTweet(username,tweetIdString)
-        
+        const sanitizedTweets = filtersTheGoodOnes(tweets)
+
+        sanitizedTweets ? tryToFavorite(sanitizedTweets) : console.log("no good tweet found 😢")
+
       } else {
         console.log(error) 
       }
+    
     })
-   
+  
   }
 
-  function showFavoriteTweet(username, tweetIdString){
-    return console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetIdString}`)
+  function filtersTheGoodOnes(tweets) {
+    
+    const arrayOfTweets = Array.from(tweets)
+
+    const containsRestrictedWords = /^((?!free|coupons|coupon).)*$/gim;
+    
+    const containsDay = /(day\d{1,2})|(day \d{1,2})|(D \d{1,2})|(D-\d{1,2})|(day-\d{1,2})|(\d{1,2}\/100+)/gim
+
+    const sanitizedTweets = arrayOfTweets.filter((tweet) => {
+      
+      return  !tweet.retweeted_status && 
+              !containsRestrictedWords.test(tweet.text) && 
+              containsDay.test(tweet.text) && 
+              tweet.entities.hashtags.length <= 5 
+    })
+
+    return sanitizedTweets
   }
-}
+
+  function tryToFavorite(tweets) {
+
+    for (let i = 0; i < tweets.length; i++) {
+
+      let tweetId = { id: tweets[i].id_str }
+      
+      client.post('favorites/create', tweetId, function (error, response) {
+        if (!error) {
+          
+          let username = response.user.screen_name;
+          let tweetIdString = response.id_str;
+
+          showFavoriteTweet(username,tweetIdString)
+          
+        } else {
+          console.log(error) 
+        }
+      })
+    
+    }
+
+    function showFavoriteTweet(username, tweetIdString){
+      return console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetIdString}`)
+    }
+  }
 
 }
 
